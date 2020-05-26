a306daf06bc131071be5885ead958063dc5b3fa4
Sebastian Brown <seb@sebbrown.net>
2020-05-26 22:48:36 +1000

    start of rebuild of Hanastian app as SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from "moment";
 
 function App() {
+  // 2014-09-25 10:15:00
+  const startDate = moment([2014,9,25]);
+  const counter = moment(startDate.diff(moment())).format("MM-DD-YYYY HH:mm:ss")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+    Hanastian
+      </div>
+     <div className="counter">
+       {counter}
+     </div>
     </div>
   );
 }
