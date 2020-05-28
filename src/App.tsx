b4e401803768a98d97949da651ff79b679353bae
Sebastian Brown <seb@sebbrown.net>
2020-05-28 22:13:16 +1000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import moment from "moment";
+import { setInterval } from 'timers';
 
 function App() {
   // 2014-09-25 10:15:00
-  const startDate = moment([2014,9,25]);
-  const counter = moment(startDate.diff(moment())).format("MM-DD-YYYY HH:mm:ss")
+  const [counter, setCounter] = React.useState("");
+  const [on, setOn] = React.useState(false);
+
+  setInterval(function () {
+    setCounter(moment([2014, 9, 25]).fromNow());
+  }, 1000);
+
   return (
     <div className="App">
       <div className="heading">
-    Hanastian
       </div>
-     <div className="counter">
-       {counter}
-     </div>
+      {/* <div className="counter">
+        {counter}
+      </div> */}
+
+
+      <div className={on ? "mover mover-on" : "mover"}>
+        <div className="floater">
+          :D
+        </div>
+      </div>
+        <button onClick={() => setOn(!on)}>Switch</button>
+      <div className="container">
+        {/* <div className="floater">
+    ?
+  </div> */}
+      </div>
     </div>
   );
 }
